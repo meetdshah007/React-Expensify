60be03379610a966ca764a4d4ff4aa8638c3c628
Meet Shah <meetdshah007@gmail.com>
2017-12-02 01:47:42 +0530

    Finalised the Routes with proper folder structure

--- src/components/EditExpensePage.jsx
+++ src/components/EditExpensePage.jsx
@@ -1,9 +1,13 @@
 import React from 'react';
 
-const EditExpensePage = ()=>(
+const EditExpensePage = (props)=>{
+  console.log(props);
+  return (
     <div>
       This is from Edit Expense.
+      Editing Page with id of {props.match.params.id}.
     </div>
-);
+  );
+}
 
 export default EditExpensePage;
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -6,7 +6,6 @@ const Header = ()=>(
       <h1>Expensify</h1>
       <NavLink to='/' exact={true} activeClassName="active-nav">Dashboard</NavLink>&nbsp;&nbsp;
       <NavLink to='/create' activeClassName="active-nav">Create Expense</NavLink>&nbsp;&nbsp;
-      <NavLink to='/edit' activeClassName="active-nav">Edit Expense</NavLink>&nbsp;&nbsp;
       <NavLink to='/help' activeClassName="active-nav">Help</NavLink>&nbsp;&nbsp;
     </header>
 );
